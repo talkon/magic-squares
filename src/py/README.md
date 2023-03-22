1b13f2ac7b7479d273b3995354964a217465a7c5
CJ Quines <cj@cjquines.com>
2023-03-22 18:08:07 -0400

    add serial option

--- src/py/README.md
+++ src/py/README.md
@@ -3,12 +3,12 @@ search for 6x6 magic squares with magic product `2^10 * 3^4 * 5^3 * 7^2`
 pypy3 rowsearch.py 10 4 3 2
 ```
 
-search for 6x6 magic squares with magic product `2^10 * 3^4 * 5^3 * 7^2` and magic sum `327`
+search for 6x6 magic squares with magic product `2^10 * 3^4 * 5^3 * 7^2` and magic sum `0` or `327`
 ```
-pypy3 rowsearch.py 10 4 3 2 --sum 327
+pypy3 rowsearch.py 10 4 3 2 --sum 0 327
 ```
 
-search for 6x6 magic squares with magic product `2^10 * 3^4 * 5^3 * 7^2` in a deterministic order
+search for 6x6 magic squares with magic product `2^10 * 3^4 * 5^3 * 7^2` in a deterministic order, without parallelism
 ```
-pypy3 rowsearch.py 10 4 3 2 --deterministic
+pypy3 rowsearch.py 10 4 3 2 --deterministic --serial
 ```
--- src/py/rowsearch.py
+++ src/py/rowsearch.py
@@ -12,33 +12,45 @@ def log(type: str, message: str) -> None:
     print(f">>> ({type}) {message}", flush=True)
 
 
-def run(N: int, P: tuple[int], S: Union[int, None], deterministic: bool = False) -> None:
+def run(
+    N: int,
+    P: tuple[int, ...],
+    SS: Union[list[int], None] = None,
+    *,
+    deterministic: bool = False,
+    serial: bool = False,
+) -> None:
     log("enum", "Generating and simplifying rows")
     enum_start = time.time()
     all_rows = gen_rows(P, N)
     row_dict = rows_to_rowdict(all_rows)
-    l = sorted(row_dict.items())
-    if not deterministic:
-        random.shuffle(l)
-    log("enum", f"number of S to consider: {len(l)}")
+    sp_sets = []
+    for S, vecs in row_dict.items():
+        if (not SS) or (S in SS):
+            sp_sets.append((S, vecs))
+    if deterministic:
+        sp_sets.sort()
+    else:
+        random.shuffle(sp_sets)
+    log("enum", f"number of S to consider: {len(sp_sets)}")
     log("enum", f"finished in {time.time()-enum_start} seconds")
 
     arrange_start = time.time()
-    if S is not None:
-        log("arrange", "Starting serial search")
-        results = [process(S, row_dict[S])]
-    else:
-        log("arrange", "Starting parallel search")
-        with multiprocessing.Pool(50) as p:
-            results = p.starmap(process, l)
+    try:
+        if serial:
+            log("arrange", "Starting serial search")
+            results = [process(S, vecs) for S, vecs in sp_sets]
+        else:
+            log("arrange", "Starting parallel search")
+            with multiprocessing.Pool(50) as p:
+                results = p.starmap(process, sp_sets)
+    finally:
+        log("arrange", f"finished in {time.time()-arrange_start} seconds")
+        log("stats", f"total time: {time.time()-enum_start} seconds")
 
     configurations = sum(result.count for result in results)
     solutions = [sol for result in results for sol in result.solutions]
     near_misses = [nm for result in results for nm in result.near_misses]
-
-    log("arrange", f"finished search for P={P} in {time.time()-arrange_start} seconds")
-
-    log("stats", f"total time: {time.time()-enum_start} seconds")
     log("stats", f"total configurations: {configurations}")
     log("stats", f"solutions found: {len(solutions)}")
     log("stats", f"solutions: {solutions}")
@@ -49,12 +61,12 @@ def run(N: int, P: tuple[int], S: Union[int, None], deterministic: bool = False)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--deterministic", action="store_true")
-    parser.add_argument("--sum", metavar="S", type=int, nargs="?")
+    parser.add_argument("--serial", action="store_true")
+    parser.add_argument("--sum", metavar="S", type=int, nargs="*")
     parser.add_argument("exponents", metavar="P", type=int, nargs="+")
     args = parser.parse_args()
 
     N = 6
     P = tuple(args.exponents)
-    S = args.sum
-    deterministic = args.deterministic
-    run(N, P, S, deterministic)
+    SS = args.sum
+    run(N, P, SS, deterministic=args.deterministic, serial=args.serial)
