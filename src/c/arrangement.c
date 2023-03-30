aff22a3ac5dc6ecf8b8219fda126c23f2bcb61c1
Steven Raphael <raphael4@mit.edu>
2023-03-30 00:59:20 -0400

    more preliminaries

--- src/c/arrangement.c
+++ src/c/arrangement.c
@@ -5,66 +5,168 @@
 #include "arrangement.h"
 
 
-int sum(row r){
+int sum(vec r){
     int s = 0;
-    for(int i = 0; i < ROW_SIZE; i++){
-        s += r.els[i];
+    for(int i = 0; i < VEC_SIZE; i++){
+        s += r.elts[i];
     }
     return s;
 }
 
-// IMPORTANT: the arrays pointed to by the returned rowgroup must be freed after use
-rowgroup read_rows(char *filename){
+// IMPORTANT: the arrays pointed to by the returned vecgroup must be freed after use
+vecgroup read_vecs(char *filename){
     FILE *fp = fopen(filename,"r");
-    int numrows = 0;
+    int numvecs = 0;
     char buf[1024];
     while(fgets(buf, 1024, fp) != NULL){
-        numrows++;
+        numvecs++;
     }
-    row* row_arr = malloc(sizeof(row) * numrows);
+    vec* vec_arr = malloc(sizeof(vec) * numvecs);
     fp = fopen(filename,"r");
-    int rowcount = 0;
+    int veccount = 0;
     int maxsum;
     while(fgets(buf, 1024, fp) != NULL){
-        row slot;
-        sscanf(buf, "%d %d %d %d %d %d", &slot.els[0], &slot.els[1], &slot.els[2], &slot.els[3], &slot.els[4], &slot.els[5]);
-        row_arr[rowcount] = slot;
-        rowcount++;
+        vec slot;
+        sscanf(buf, "%d %d %d %d %d %d", &slot.elts[0], &slot.elts[1], &slot.elts[2], &slot.elts[3], &slot.elts[4], &slot.elts[5]);
+        vec_arr[veccount] = slot;
+        veccount++;
         maxsum = sum(slot);
     }
     int cursum = 0;
     // all infos are initialized to 0 length and null starts
-    rowgroupinfo* infos = calloc(maxsum + 1, sizeof(rowgroupinfo));
-    for(int i = 0; i < numrows; i++){
-        row currow = row_arr[i];
-        if(sum(currow) != cursum){
-            infos[sum(currow)].start = row_arr + i;
+    vecgroupinfo* infos = calloc(maxsum + 1, sizeof(vecgroupinfo));
+    for(int i = 0; i < numvecs; i++){
+        vec curvec = vec_arr[i];
+        if(sum(curvec) != cursum){
+            infos[sum(curvec)].start = vec_arr + i;
         }
-        infos[sum(currow)].length++;
-        cursum = sum(currow);
+        infos[sum(curvec)].length++;
+        cursum = sum(curvec);
     }
-    rowgroup r;
-    r.rows = row_arr;
+    vecgroup r;
+    r.vecs = vec_arr;
     r.infos = infos;
-    r.numrows = numrows;
+    r.numvecs = numvecs;
     r.numsums = maxsum + 1;
     return r;
 
 }
 
-void test_print_rows(char *filename){
-    rowgroup group = read_rows(filename);
+int index_cmp_reverse(const void *a, const void *b){
+    int v1 = (*(indexed_int*)a).freq, v2 = (*(indexed_int*)b).freq;
+    return (v1 < v2) - (v1 > v2);
+}
+
+int int_reverse(const void *a, const void *b){
+    return (*(int*)a < *(int*)b) - (*(int*)a > *(int*)b);
+}
+
+int vec_reverse(const void *a, const void *b){
+    int v1 = (*((vec*)a)).elts[0], v2 = (*((vec*)b)).elts[0];
+    return (v1 < v2) - (v1 > v2);
+}
+
+// free relabeling pointers after use
+relabeling elt_relabeling(vecgroup group, int sum){
+    vec* vec_arr = group.infos[sum].start;
+    int total_vecs = group.infos[sum].length;
+    relabeling r;
+    indexed_int* elt_freqs = calloc(sum, sizeof(indexed_int));
+    for(int i = 0; i < sum; i++){
+        elt_freqs[i].elt = i;
+    }
+    for(int i = 0; i < total_vecs; i++){
+        for(int j = 0; j < VEC_SIZE; j++){
+            elt_freqs[vec_arr[i].elts[j]].freq++;
+        }
+    }
+    qsort(elt_freqs, sum, sizeof(indexed_int), index_cmp_reverse);
+    int* elt_to_label = malloc(sum * sizeof(int));
+    int* label_to_elt = malloc(sum * sizeof(int));
+    for(int i = 0; i < sum; i++){
+        elt_to_label[elt_freqs[i].elt] = i;
+        label_to_elt[i] = elt_freqs[i].elt;
+    }
+    vec* new_vecs = malloc(total_vecs * sizeof(vec));
+    for(int i = 0; i < total_vecs; i++){
+        for(int j = 0; j < VEC_SIZE; j++){
+            new_vecs[i].elts[j] = elt_to_label[vec_arr[i].elts[j]];
+        }
+        qsort(&(new_vecs[i].elts[0]), VEC_SIZE, sizeof(int), int_reverse);
+    }
+    qsort(new_vecs, total_vecs, sizeof(vec), vec_reverse);
+    r.vecs = new_vecs;
+    r.label_to_elt = label_to_elt;
+    free(elt_to_label);
+    free(elt_freqs);
+    return r;
+}
+
+int num_inters(vec a, vec b){
+    int p1 = 0;
+    int p2 = 0;
+    int tot = 0;
+    while(p1 < VEC_SIZE && p2 < VEC_SIZE){
+        if(a.elts[p1] > a.elts[p2]){
+            p1++;
+        }
+        else{
+            if(a.elts[p1] == a.elts[p2]){
+                p1++;
+                tot++;
+            }
+            p2++;
+        }
+    }
+    return tot;
+}
+
+unsigned char** intersections(vec* vecs, int numvecs){
+    unsigned char** inters = malloc(numvecs * sizeof(char*));
+    for(int i = 0; i < numvecs; i++){
+        inters[i] = malloc(numvecs * sizeof(char));
+        for(int j = 0; j < numvecs; j++){
+            inters[i][j] = num_inters(vecs[i], vecs[j]);
+        }
+    }
+    return inters;
+}
+
+void search_sum(vecgroup group, int sum){
+    vec* start = group.infos[sum].start;
+    int total_vecs = group.infos[sum].length;
+    if(total_vecs == 0){
+        return;
+    }
+    relabeling r = elt_relabeling(group, sum);
+    unsigned char** inters = intersections(r.vecs, total_vecs);
+    int* valid_indices = malloc(total_vecs * sizeof(int));
+    for(int i = 0; i < total_vecs; i++){
+        valid_indices[i] = i;
+    }
+    int num_valid_vecs = total_vecs;
+    free(inters);
+    free(r.vecs);
+    free(r.label_to_elt);
+    free(valid_indices);
+}
+
+void test_print_vecs(char *filename){
+    vecgroup group = read_vecs(filename);
     for(int i = 0; i < group.numsums; i++){
         printf("%d\n", group.infos[i].length);
     }
-    free(group.rows);
+    search_sum(group, 327);
+    free(group.vecs);
     free(group.infos);
 }
+
+
 
 int main(int argc, char *argv[]){
     int opt;
     char filename[1024];
-    while ((opt = getopt (argc, argv, "f:")) != -1){
-        test_print_rows(optarg);
+    while ((opt = getopt(argc, argv, "f:")) != -1){
+        test_print_vecs(optarg);
     }
 }
--- src/c/arrangement.h
+++ src/c/arrangement.h
@@ -1,17 +1,46 @@
-#define ROW_SIZE 6
+#define VEC_SIZE 6
 
-typedef struct row {
-    int els[ROW_SIZE];
-} row;
+typedef struct vec {
+    int elts[vec_SIZE];
+} vec;
 
-typedef struct rowgroupinfo {
-    row* start;
+typedef struct vecgroupinfo {
+    vec* start;
     int length;
-} rowgroupinfo;
+} vecgroupinfo;
 
-typedef struct rowgroup {
-    row* rows;
-    rowgroupinfo* infos;
-    int numrows;
+typedef struct vecgroup {
+    vec* vecs;
+    vecgroupinfo* infos;
+    int numvecs;
     int numsums;
-} rowgroup;
+} vecgroup;
+
+typedef struct relabeling {
+    vec* vecs;
+    int* label_to_elt;
+} relabeling;
+
+typedef struct search_table {
+    vec rows[6];
+    vec cols[6];
+    int numrows;
+    int numcols;
+    int* valid_rows;
+    int num_valid_rows;
+    int* valid_cols;
+    int num_valid_cols;
+} search_table;
+
+typedef struct indexed_int {
+    int elt;
+    int freq;
+} indexed_int;
+
+int sum(vec r);
+
+vecgroup read_vecs(char *filename);
+
+void search_sum(vecgroup group, int sum);
+
+void test_print_vecs(char *filename);
