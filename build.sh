350131621d368242aad894f0d32eb3ac5b6d8436
CJ Quines <cj@cjquines.com>
2023-04-07 15:46:20 -0400

    add clean to build

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
-# usage: ./build.sh or ./build.sh d
+# usage:
+#   ./build.sh: build release version
+#   ./build.sh d: build debug version
+#   ./build.sh c: clean release build
+#   ./build.sh cd: clean debug build
 
 if [[ $* == *d* ]]; then
   : ${CMAKE_DIR:=cmake-build-debug}
+  cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo -S . -B $CMAKE_DIR
 else
   : ${CMAKE_DIR:=cmake-build-release}
+  cmake -D CMAKE_BUILD_TYPE=Release -S . -B $CMAKE_DIR
 fi
 
-cmake -D CMAKE_BUILD_TYPE=Release -S . -B $CMAKE_DIR
-cmake --build $CMAKE_DIR
+if [[ $* == *c* ]]; then
+  cmake --build $CMAKE_DIR --target clean
+else
+  cmake --build $CMAKE_DIR
+fi
 
 mkdir -p bin
 ln -sf $(realpath $CMAKE_DIR)/src/c/arrangement bin/arrangement
