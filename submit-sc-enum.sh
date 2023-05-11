738a6a09629441dc2538cb65ce8c4012228ff5b9
Krit Boonsiriseth <talkon@mit.edu>
2023-05-11 01:18:41 -0400

    Create submit-sc-enum.sh

---
+++ submit-sc-enum.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# usage: INPUT=[input file] COUNT=[count] LLsub ./submit-sc.sh [LLsub options]
+# input file format: each line contains a P tuple
+
+source /etc/profile
+module add anaconda/2023a
+
+export PYPY3=python
+P_TUPLE=$(head "-$((1 + $LLSUB_RANK))" $INPUT | tail -1)
+
+echo ./run.sh "$P_TUPLE" -e
+./run.sh "$P_TUPLE" -e
+
